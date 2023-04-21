9e1048582d7ff29eaebb78d02c7d5668377b7812
MoamenSherif <moamenyt@gmail.com>
2023-04-21 13:27:56 +0200

    finishing the airbnb project

--- project2-airbnb/src/App.js
+++ project2-airbnb/src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Card from "./components/card";
 import Hero from "./components/hero";
+import firstImg from "./images/image 12.png";
+import secondImg from "./images/wedding-photography 1.png";
+import thirdImg from "./images/mountain-bike 1.png";
 
 function App() {
   return (
@@ -8,9 +11,33 @@ function App() {
       <Navbar />
       <Hero />
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          img={firstImg}
+          rate="5.0"
+          reviewCount={6}
+          country="USA"
+          title="Life lessons with Katie Zaferes"
+          price="136"
+          available={false}
+        />
+        <Card
+          img={secondImg}
+          rate="5.0"
+          reviewCount={30}
+          country="USA"
+          title="Learn wedding photography"
+          price="125"
+          available={true}
+        />
+        <Card
+          img={thirdImg}
+          rate="4.8"
+          reviewCount={2}
+          country="USA"
+          title="Group Mountain Biking"
+          price="50"
+          available={false}
+        />
       </div>
     </div>
   );
--- project2-airbnb/src/components/card.jsx
+++ project2-airbnb/src/components/card.jsx
@@ -1,28 +1,27 @@
-import cardImg from '../images/image 12.png';
 import starImg from '../images/Star 1.png';
 
 
-function Card(){
+function Card(props){
   return (
     <div className="card">
       <div className="card__top">
-        <img src={cardImg} alt="" className="card__img" />
+        <img src={props.img} alt="" className="card__img" />
         <div className="card__availability">
-          SOLD OUT
+          {props.available ? "ONLINE" : "SOLD OUT"}
         </div>
       </div>
       <div className="card__info">
         <div className="card__reviews">
           <img src={starImg} alt="" />
-          <span className='card__reviews-rate'>5.0</span>
-          <span className='card__reviews-count'>(6)</span>.
-          <span className='card__reviews-location'>USA</span>
+          <span className='card__reviews-rate'>{props.rate}</span>
+          <span className='card__reviews-count'>({props.reviewCount})</span>.
+          <span className='card__reviews-location'>{props.country}</span>
         </div>
         <div className="card__description">
-          Life lessons with Katie Zaferes
+          {props.title}
         </div>
         <div className="card__price">
-          <span className="card__price--bold">From $136</span> / person
+          <span className="card__price--bold">From ${props.price}</span> / person
         </div>
       </div>
     </div>
