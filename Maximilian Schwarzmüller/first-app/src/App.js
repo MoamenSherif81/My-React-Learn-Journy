0ae84ca6e9d20a32fabfe6bc6689e578b6bc5a04
Moamen Sherif <102827938+MoamenSherif81@users.noreply.github.com>
2023-05-18 14:22:33 +0300

    split code into some components

--- Maximilian Schwarzmüller/first-app/src/App.js
+++ Maximilian Schwarzmüller/first-app/src/App.js
@@ -1,29 +1,34 @@
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import ExpensesList from "./components/ExpensesList/ExpensesList";
 
 function App() {
+  const expenses = [
+    {
+      title: 'Frontend udemy course',
+      price: 297,
+      date: new Date("3/5/2023")
+    },
+    {
+      title: 'Codeforces GYM',
+      price: 45,
+      date: new Date("3/5/2023")
+    },
+    {
+      title: '2 USB flash drive',
+      price: 300,
+      date: new Date("1/5/2023")
+    },
+    {
+      title: 'Laptop holder',
+      price: 139,
+      date: new Date("2/5/2023")
+    }
+  ]
+
   return (
     <div className="App">
       <h2>Our App</h2>
-      <ExpenseItem
-        title="Frontend udemy course"
-        price="297"
-        date={new Date("3/5/2023")}
-      />
-      <ExpenseItem
-        title="Codeforces GYM"
-        price="45"
-        date={new Date("3/5/2023")}
-      />
-      <ExpenseItem
-        title="2 USB flash drive"
-        price="300"
-        date={new Date("1/5/2023")}
-      />
-      <ExpenseItem
-        title="Laptop holder"
-        price="139"
-        date={new Date("2/5/2023")}
-      />
+      <ExpensesList expenses={expenses} />
     </div>
   );
 }
--- Maximilian Schwarzmüller/first-app/src/components/ExpenseItem/ExpenseItem.jsx
+++ Maximilian Schwarzmüller/first-app/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,15 +1,12 @@
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
   
   return (
     <div className='expense-item'>
-      <div className='expense-item__date'>
-        <div>{props.date.toLocaleString('en-us', {month: 'long'})}</div>
-        <div>Year</div>
-        <div>Day</div>
-      </div>
-      <div className='expense-item__info'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2 className='expense-item__title'>{props.title}</h2>
         <div className='expense-item__price'>{props.price} EGP</div>
       </div>
